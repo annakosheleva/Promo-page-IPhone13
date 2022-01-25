775f667680e2c02e27561fc1f1f9daa12e492bde
annakosheleva <71738312+annakosheleva@users.noreply.github.com>
2022-01-25 15:13:04 +0300

    feat: add send form

--- scripts/accordeon.js
+++ scripts/accordeon.js
@@ -1,21 +1,38 @@
 const accordeon = () => {
-  const chItems = document.querySelectorAll(".characteristics__item");
+	const chitems = document.querySelectorAll(".characteristics__item");
 
-  chItems.forEach((item) => {
-    const chButton = item.querySelector(".characteristics__title");
-    const chContent = item.querySelector(".characteristics__description");
+	chitems.forEach((item, index) => {
+		const chButton = item.querySelector(".characteristics__title");
 
-    chButton.addEventListener("click", () => {
-      if (chContent.classList.contains("open")) {
-        chContent.style.height = "";
-      } else {
-        chContent.style.height = chContent.scrollHeight + "px";
-      }
+		const toggleAccordeonItem = (item) => {
+			const chButton = item.querySelector(".characteristics__title");
+			const chContent = item.querySelector(
+				".characteristics__description"
+			);
+			if (chContent.classList.contains("open")) {
+				chContent.style.height = "";
+			} else {
+				chContent.style.height = chContent.scrollHeight + "px";
+			}
+			chButton.classList.toggle("active");
+			chContent.classList.toggle("open");
+		};
 
-      chButton.classList.toggle("active");
-      chContent.classList.toggle("open");
-    });
-  });
+		chButton.addEventListener("click", () => {
+			chitems.forEach((closeItem, closeIndex) => {
+				if (
+					closeItem
+						.querySelector(".characteristics__description")
+						.classList.contains("open") &&
+					index != closeIndex
+				) {
+					toggleAccordeonItem(closeItem);
+				}
+			});
+
+			toggleAccordeonItem(item);
+		});
+	});
 };
 
 accordeon();
